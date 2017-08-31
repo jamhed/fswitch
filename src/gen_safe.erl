5333226c6eb8c0f0025b57679995a2c2e04da180
Roman Galeev <roman.galeev@ezuce.com>
2017-08-31 20:13:12 +0200

    Add safe wrappers for subscribe/unsub

--- src/gen_safe.erl
+++ src/gen_safe.erl
@@ -1,5 +1,7 @@
 -module(gen_safe).
+-include_lib("stdlib/include/qlc.hrl").
 -export([call/3, call/4, cast/3, info/3]).
+-export([subscribe/1, unsubscribe/1, subscriptions/1, subscriptions/2]).
 
 call(Id, _PidF, Msg) when is_pid(Id) -> gen_server:call(Id, Msg);
 call(Id, PidF, Msg) ->
@@ -28,3 +30,26 @@ info(Id, PidF, Msg) ->
 		undefined -> {error, no_pid, Id};
 		Pid -> Pid ! Msg
 	end.
+
+% allow multiple calls to subscribe from the same pid
+
+subscribe(K) ->
+	case subscriptions(K, self()) of
+		[] -> gproc:reg({p, l, K}, subscribe);
+		_ -> skip
+	end.
+
+unsubscribe(K) ->
+	Self = self(),
+	case subscriptions(K, Self) of
+		[Self] -> gproc:unreg({p, l, K});
+		_ -> skip
+	end.
+
+subscriptions(K) ->
+	Q = qlc:q([ Pid || {{p, l, Ks}, Pid, subscribe} <- gproc:table({l, p}), Ks =:= K ]),
+	qlc:e(Q).
+
+subscriptions(K, P) ->
+	Q = qlc:q([ Pid || {{p, l, Ks}, Pid, subscribe} <- gproc:table({l, p}), Ks =:= K, Pid =:= P ]),
+	qlc:e(Q).
