ec817154209507afd2c4995b698751d6d7c35b0a
Roman Galeev <roman.galeev@ezuce.com>
2017-09-03 03:25:49 +0200

    Remove unused call

--- src/gen_safe.erl
+++ src/gen_safe.erl
@@ -1,6 +1,6 @@
 -module(gen_safe).
 -include_lib("stdlib/include/qlc.hrl").
--export([call/3, call/4, cast/3, info/3]).
+-export([call/3, call/4, cast/3]).
 -export([subscribe/1, unsubscribe/1, subscriptions/1, subscriptions/2]).
 
 call(Id, _PidF, Msg) when is_pid(Id) -> gen_server:call(Id, Msg);
@@ -22,13 +22,6 @@ cast(Id, PidF, Msg) ->
 	case PidF(Id) of
 		undefined -> {error, no_pid, Id};
 		Pid -> gen_server:cast(Pid, Msg)
-	end.
-
-info(Id, _PidF, Msg) when is_pid(Id) -> gen_server:info(Id, Msg);
-info(Id, PidF, Msg) ->
-	case PidF(Id) of
-		undefined -> {error, no_pid, Id};
-		Pid -> Pid ! Msg
 	end.
 
 % allow multiple calls to subscribe from the same pid
