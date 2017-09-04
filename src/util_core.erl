06132e69d2833202511dafd6baf7c9c716b6754d
Roman Galeev <roman.galeev@ezuce.com>
2017-09-04 05:18:56 +0200

    Add map flatten/explode functions

--- src/util_core.erl
+++ src/util_core.erl
@@ -1,9 +1,20 @@
 -module(util_core).
--export([match_maps/2, seconds_from/1, ns_from/1, ms_from/1]).
+-export([match_maps/2, seconds_from/1, ns_from/1, ms_from/1, flatten/1, explode/1]).
+
+flatten(M) -> lists:flatten(flatten([], M)).
+
+flatten(Pk, M) when is_map(M) ->
+	[ flatten(Pk++[K], V) || {K, V} <- maps:to_list(M) ];
+flatten(K, V) ->
+	{K, V}.
+
+explode(L) -> lists:foldl(fun({K,V}, M) -> explode(K, V, M) end, #{}, L).
+explode([K], V, M) -> M#{ K => V };
+explode([K|Rest], V, M) -> M#{ K => explode(Rest, V, maps:get(K, M, #{})) }.
 
 match_maps(_, undefined) -> false;
 match_maps(Inner, Outer) ->
-	L = maps:to_list(Inner) -- maps:to_list(Outer),
+	L = flatten(maps:to_list(Inner)) -- flatten(maps:to_list(Outer)),
 	case erlang:length(L) of
 		0 -> true;
 		_N -> false
